4dd7e5a6faf8f32c5c5cef6c5b46d6a7c6061175
Lakshit Agarwal <lakshitcollege@gmail.com>
2025-06-15 02:36:05 +0530

    Add CTA componen and  Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import DemoSection from "@/components/Landing-page-components/DemoSection";
 import HeroSection from "@/components/Landing-page-components/HeroSection";
 import HowItWorks from "@/components/Landing-page-components/HowItWorks";
+import CTA from "@/components/Landing-page-components/CTA";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <DemoSection />
       <HowItWorks />
+      <CTA />
     </div>
   );
 }
---
+++ components/Landing-page-components/CTA.tsx
@@ -0,0 +1,27 @@
+import { Button } from "../ui/button";
+
+export default function CTA() {
+  return (
+    <div className="text-center py-16">
+      <h1 className="text-4xl font-extrabold">
+        Done Writing Docs? Let AI Take Over.
+      </h1>
+      <h2 className="text-gray-600 py-4 w-1/3 mx-auto">
+        Instantly turn your GitHub repo into a polished README. Save time, stay
+        focused on building.
+      </h2>
+      <Button
+        variant="outline"
+        className="h-10 px-6 py-2.5 rounded-lg text-base
+              bg-gradient-to-r from-rose-100 to-pink-100 
+              text-rose-600 hover:text-rose-600 font-medium transition-all duration-200
+              border border-rose-200/50 
+              hover:bg-rose-50 hover:border-rose-500
+              active:bg-rose-100
+              cursor-pointer shadow-lg shadow-rose-200"
+      >
+        🚀 Generate My README →
+      </Button>
+    </div>
+  );
+}
