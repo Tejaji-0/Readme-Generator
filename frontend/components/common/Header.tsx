d04a94dff292bf9b56846e723dfe4c7093e10a31
Lakshit Agarwal <lakshitcollege@gmail.com>
2025-08-26 20:48:19 +0530

    minor css changes

--- frontend/components/common/Header.tsx
+++ frontend/components/common/Header.tsx
@@ -4,14 +4,14 @@ import { GitHubStarsButton } from "@/components/animate-ui/buttons/github-stars"
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center p-10 md:px-24">
+    <div className="flex justify-between items-center py-10 px-5 md:px-24">
       <Link href="/">
         <div className="flex items-center gap-2">
           <FileText
             size={36}
             className="hover:rotate-12 tranform transition-all duration-200 ease-in-out "
           />
-          <span className="font-extrabold text-[22px] text-gray-900">
+          <span className="hidden md:block font-extrabold text-[22px] text-gray-900">
             Readme.ai
           </span>
         </div>
